954373c459451dae9880894f9cb49ce0c7d8b5fb
Samantha Wong <smiwong920@gmail.com>
2019-01-26 23:30:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,18 +1,18 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++)  {
-    if (i===1) {
-      array.push("I am " + i + " strange loop.")
-  } else {
-  array.push("I am " + i + " strange loops.") }
+    if (i===1)
+    {array.push("I am " + i + " strange loop.")} 
+    else 
+    {array.push("I am " + i + " strange loops.") }
   }
   return array
 }
 
 function whileLoop(number) {
-  let x = 0 
-  while (x<5) {
-    console.log(x)
-    x++
+  
+  while (number>0) 
+  {console.log(number)
+    number--
   }
   return 'done'
 }
